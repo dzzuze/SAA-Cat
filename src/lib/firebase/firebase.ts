8acad4a9804da0b9a7e7f60aaffc59c080e98f80
Thanatus <s.sidoryaka@gmail.com>
2026-02-28 23:41:58 +0200

    feat: install zustand

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { firebaseConfig } from "./config";
 
 export const appFireBase = initializeApp(firebaseConfig);
+export const auth = getAuth(appFireBase);
