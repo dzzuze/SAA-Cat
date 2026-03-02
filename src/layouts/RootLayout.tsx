f313be0640306514e659bf0f139c28fcbe582140
Thanatus <s.sidoryaka@gmail.com>
2026-03-02 22:49:11 +0200

    chore: delete .env

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header/Header";
 
 export default function RootLayout() {
   return (
     <div className="min-h-screen flex flex-col bg-[#18181b] text-white">
-      <Header />
-
       <main className="flex-1 flex items-center justify-center">
         <Outlet />
       </main>
