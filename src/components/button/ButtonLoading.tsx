87899d24c855e03217570bd5f291932a63239512
dzzuze <notinsidik@gmail.com>
2026-03-10 23:41:24 +0300

    Update and rename btn-loading.tsx to ButtonLoading.tsx

--- src/components/button/ButtonLoading.tsx
+++ src/components/button/ButtonLoading.tsx
@@ -7,13 +7,15 @@ const ButtonLoading = ({
   loading,
   children,
   className,
+  type = "button",
   ...props
 }: ButtonProps) => {
   return (
     <button
+      type={type}
       {...props}
       disabled={loading || props.disabled}
-      className={`w-full bg-yellow-400 hover:bg-yellow-400 text-black font-bold py-3 rounded-xl transition-all shadow-[0_0_20px_rgba(234,179,8,0.2)] hover:shadow-[0_0_25px_rgba(234,179,8,0.4)] active:scale-[0.98] disabled:opacity-70 ${className}`}
+      className={`w-full bg-main-yellow hover:bg-main-yellow text-black font-bold py-3 rounded-xl transition-all shadow-[0_0_20px_rgba(234,179,8,0.2)] hover:shadow-[0_0_25px_rgba(234,179,8,0.4)] active:scale-[0.98] disabled:opacity-70 ${className}`}
     >
       {loading ? "Loading..." : children}
     </button>
