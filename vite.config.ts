048c65f5a0f39eb45fe13d593ecb1da7a191f93f
Thanatus <s.sidoryaka@gmail.com>
2026-03-12 12:51:09 +0200

    chore: install vitest, jsdom, RTL

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -15,4 +14,10 @@ export default defineConfig({
     }),
   ],
   base: "/SAA-Cat/",
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    css: true,
+  },
 });
