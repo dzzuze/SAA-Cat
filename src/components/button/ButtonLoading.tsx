61a899935872cc1865d9bb961e9006996e60b2a3
dzzuze <notinsidik@gmail.com>
2026-03-18 02:06:38 +0300

    chore: add cursor into button loading

--- src/components/button/ButtonLoading.tsx
+++ src/components/button/ButtonLoading.tsx
@@ -15,7 +15,7 @@ const ButtonLoading = ({
       type={type}
       {...props}
       disabled={loading || props.disabled}
-      className={`w-full bg-main-yellow hover:bg-main-yellow text-black font-bold py-3 rounded-xl transition-all shadow-[0_0_20px_rgba(234,179,8,0.2)] hover:shadow-[0_0_25px_rgba(234,179,8,0.4)] active:scale-[0.98] disabled:opacity-70 ${className}`}
+      className={`w-full bg-main-yellow hover:bg-main-yellow text-black cursor-pointer font-bold py-3 rounded-xl transition-all shadow-[0_0_20px_rgba(234,179,8,0.2)] hover:shadow-[0_0_25px_rgba(234,179,8,0.4)] active:scale-[0.98] disabled:opacity-70 ${className}`}
     >
       {loading ? "Loading..." : children}
     </button>
