eec217bf14f3a48cb0a6dd30dd3be5bece882fb8
Thanatus <s.sidoryaka@gmail.com>
2026-03-17 12:46:24 +0200

    chore: add user type

---
+++ src/types/users.ts
@@ -0,0 +1,17 @@
+export interface IUserProfile {
+  uid: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  nickName: string;
+
+  role: string;
+  createdAt: string;
+  stats: IUserStats;
+  completed: string[];
+}
+
+interface IUserStats {
+  completedQuizzes: number;
+  points: number;
+}
