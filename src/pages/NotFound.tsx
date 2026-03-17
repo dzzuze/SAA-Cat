5c76d634cd745ac9786c7245c5cb790f6240678f
dzzuze <notinsidik@gmail.com>
2026-03-18 02:09:34 +0300

    feat: improve 404 styles

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,8 +3,8 @@ import LostCat from "../assets/lost_cat404.svg?react";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center py-16 text-center">
-      <LostCat className="mb-6 h-48 w-48" />
+    <div className="flex flex-col flex-1 items-center justify-center py-16 text-center">
+      <LostCat className="mb-2 h-40 w-48" />
 
       <h1 className="text-6xl font-bold text-gray-800">404</h1>
       <p className="mt-3 text-lg text-gray-600">
@@ -13,7 +13,7 @@ export default function NotFound() {
 
       <Link
         to="/"
-        className="mt-6 rounded-xl bg-black px-6 py-3 text-white transition hover:bg-gray-800"
+        className="mt-6 rounded-xl bg-black font-bold px-6 py-3 text-white transition hover:bg-yellow-400 hover:text-black"
       >
         Go back home
       </Link>
