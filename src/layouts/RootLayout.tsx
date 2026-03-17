956d788dd4fedfc8b0e116333d1d5dc486942a68
dzzuze <notinsidik@gmail.com>
2026-03-18 02:08:09 +0300

    fix: fix the display

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout() {
   return (
     <div className="min-h-screen flex flex-col overflow-hidden">
       <Header />
-      <main className="flex  flex-1  bg-[#1a1a1d] text-white" />
+      <main className="flex bg-[#1a1a1d] text-white" />
       <Outlet />
       <Footer />
     </div>
