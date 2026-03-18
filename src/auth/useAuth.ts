826e6c373761e4c3a694948b9fe19cacfcd338ee
Thanatus <s.sidoryaka@gmail.com>
2026-03-18 15:22:27 +0200

    feat: impoving users store and user auth hook

--- src/auth/useAuth.ts
+++ src/auth/useAuth.ts
@@ -2,7 +2,15 @@ import { useAuthStore } from "../store/authStore";
 
 export const useAuth = () => {
   const user = useAuthStore((s) => s.user);
+  const profile = useAuthStore((s) => s.profile);
   const loading = useAuthStore((s) => s.loading);
-
-  return { user, loading };
+  const logout = useAuthStore((s) => s.logout);
+  const isAuth = !!user;
+  return {
+    user,
+    profile,
+    loading,
+    isAuth,
+    logout,
+  };
 };
--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,17 +1,32 @@
 import { create } from "zustand";
 import type { User } from "firebase/auth";
+import type { IUserProfile } from "../types/users";
 
 type AuthState = {
   user: User | null;
+  profile: IUserProfile | null;
   loading: boolean;
   setUser: (user: User | null) => void;
+  setProfile: (profile: IUserProfile | null) => void;
   setLoading: (loading: boolean) => void;
+  updateProfile: (partialProfile: Partial<IUserProfile>) => void;
+  logout: () => void;
 };
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
+  profile: null,
   loading: true,
 
   setUser: (user) => set({ user }),
+
+  setProfile: (profile) => set({ profile }),
+
   setLoading: (loading) => set({ loading }),
+  updateProfile: (partialProfile) =>
+    set((state) => ({
+      profile: state.profile ? { ...state.profile, ...partialProfile } : null,
+    })),
+
+  logout: () => set({ user: null, profile: null, loading: false }),
 }));
